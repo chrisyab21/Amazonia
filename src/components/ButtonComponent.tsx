ef236298416f75c94f03acdbb6f68e6b2b65cd67
christian <christianyabeta@gmail.com>
2024-07-09 12:18:44 -0400

    Mejoras en colores, modificacion de icono de la pagina en la pestaña

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -14,7 +14,7 @@ export const ButtonComponent: FC<props> = ({ disabled }) => {
     return (
         <button
             disabled={disabled}
-            className={`w-full basis-14 flex-shrink-0 rounded-md bg-blue-700 disabled:bg-gray-800 disabled:hover:bg-green-800 hover:bg-green-700  enabled:focus:bg-green-700 
+            className={`w-full basis-14 flex-shrink-0 rounded-md bg-blue-900 bg-opacity-80 disabled:bg-gray-800 disabled:hover:bg-green-800 hover:bg-green-700  enabled:focus:bg-green-700 
             ${styles['button-active']} transition duration-300 ease-in-out ${styles['custom-shadow']} ${styles['button-hover']} 
             focus:outline-none text-lg text-gray-200 font-rubik disabled:text-gray-400 tracking-wide`}
         >
--- src/components/MissionSection.tsx
+++ src/components/MissionSection.tsx
@@ -6,8 +6,8 @@ export const MissionSection = () => {
   return (
     <section id="mission" className="flex justify-center items-center bg-AzulOscuro p-20 px-14 sm:px-16 md:px-20 gap-10 md:gap-6 lg:gap-20">
       <div className=" flex flex-col justify-center items-center gap-8 md:w-10/12 lg:w-6/12">
-        <h5 className={`mb-2 font-roboto font-semibold text-2xl text-center tracking-widest text-gray-900 dark:text-white ${styles['text-shadow']}`}>MISION</h5>
-        <p className="mb-3 font-roboto font-normal text-justify text-lg text-gray-700 leading-10 dark:text-gray-400">
+        <h5 className={`mb-2 font-roboto font-semibold text-2xl text-center tracking-widest text-white dark:text-white ${styles['text-shadow']}`}>MISION</h5>
+        <p className="mb-3 font-roboto font-normal text-justify text-lg text-gray-300 leading-10 dark:text-gray-300">
           Significantly contribute to the fight against climate change through the implementation of REDD+ projects (Reducing Emissions from Deforestation and Forest Degradation), with the goal of mitigating greenhouse gas emissions and promoting environmental sustainability.&ensp;
           <br className="md:hidden" />
           <br className="md:hidden" />
